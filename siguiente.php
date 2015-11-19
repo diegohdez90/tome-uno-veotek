5ee8141c2ddac898cf7b81c5edb5b071f1b50819
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2015-11-19 11:24:05 -0600

    Tome Uno
    Se cambio la interfaz de pantalla de punto de venta.

--- siguiente.php
+++ siguiente.php
@@ -7,7 +7,6 @@
 <link rel="stylesheet" href="css/print.css" type="text/css" media="print" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script type="text/javascript" src="js/main.js"></script>
-<META HTTP-EQUIV="REFRESH" CONTENT="5;URL=siguiente.php">
 </head>
 
 
@@ -33,14 +32,29 @@
 <body>
 	<div class="container">
 		<div class="row turno">
-			<h3 class="text-center">Turno:</h3>
-			<p>
-				<h2 class="text-center"><?php echo $id; ?></h2>
-				<!--<form method="post" action="atendido.php">
-					<input type="hidden" name="id" value="<?php echo $id; ?>">
-					<input type="submit">
-				</form>-->
-			</p>
+			<img class="veotek" src="img/veotek.png" width="90%">
+		</div>
+		<div class="row turno">
+			<div class="col-md-2 l">
+				<img src="img/veotek.png" width="150%">
+			</div>
+			<div class="col-md-8 turno">
+				<h3 class="text-center">Turno:</h3>
+				<p>
+					<h2 class="text-center"><?php echo $id; ?></h2>
+					<!--<form method="post" action="atendido.php">
+						<input type="hidden" name="id" value="<?php echo $id; ?>">
+						<input type="submit">
+					</form>-->
+				</p>
+
+			</div>
+			<div class="col-md-2 r">
+				<img  src="img/veotek.png" width="150%">
+			</div>
+		</div>
+		<div class="row turno">
+			<img class="veotek" src="img/veotek.png" width="90%">
 		</div>
 	</div>
 </body>
